fbd6c1d1775674f6147ea7faef8ee4e3f2526d04
Men Nguyen <nvmen@tma.com.vn>
2018-06-08 17:25:01 +0700

    add example source code

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
+
+console.disableYellowBox = true;
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppWithNavigationState listener={createReduxBoundAddListener('root')} />
+      </Provider>
+    );
+  }
+}
